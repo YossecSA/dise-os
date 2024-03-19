673992cb53cd95529867837bf487d33ea96f5a29
Yossec <yosec.arratea22@gmail.com>
2024-03-18 20:26:53 -0500

    funcionamiento al 100%

--- efecto_escritura/script.js
+++ efecto_escritura/script.js
@@ -1,5 +1,14 @@
-var typed = new Typed('.auto-type', {
-    strings: ['sentence.', '&amp; a second sentence.'],
-    typeSpeed: 150,
-    backSpeed: 150
-});
+// var typed = new Typed('.auto-type', {
+//     strings: ['sentence.', '&amp; a second sentence.'],
+//     typeSpeed: 150,
+//     backSpeed: 150
+// });
+//}
+ var options = {
+    strings: ['Texto 1', 'Texaato 2', 'Texddto 3'], // Array de textos a mostrar
+    typeSpeed: 150, // Velocidad de mecanografía en milisegundos
+    backSpeed: 150, // Velocidad de mecanografía en retroceder
+    loop: true // Opción para hacer bucle del texto
+  };
+
+  var typed = new Typed('.auto-type', options);
